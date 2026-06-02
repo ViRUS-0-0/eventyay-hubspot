914c8f8740736fc00c8805e60263517297eec39b
ViRUS-0-0 <mrvanwariomg@gmail.com>
2026-06-02 20:25:12 +0530

    plugin initialization with cookiecutter

---
+++ eventyay_hubspot/apps.py
@@ -0,0 +1,25 @@
+from django.utils.translation import gettext_lazy as _
+
+from . import __version__
+
+try:
+    from eventyay.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use a later version of eventyay-tickets")
+
+
+class Eventyay Hubspot PluginApp(PluginConfig):
+    default = True
+    name = "eventyay_hubspot"
+    verbose_name = _("Eventyay Hubspot Plugin")
+
+    class EventyayPluginMeta:
+        name = _("Eventyay Hubspot Plugin")
+        author = "Om Vanwari"
+        description = _("This plugin allows you to integrate Eventyay with Hubspot")
+        visible = True
+        version = __version__
+        category = "INTEGRATION"
+
+    def ready(self):
+        from . import signals  # NOQA
