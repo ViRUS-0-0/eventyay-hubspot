224797836ab62be86a1dbd853bd083524e8bce06
ViRUS-0-0 <mrvanwariomg@gmail.com>
2026-06-03 19:30:45 +0530

    feat(tickets-settings): added a signal for new tab named hubspot

--- hubspot/signals.py
+++ hubspot/signals.py
@@ -1,1 +1,19 @@
-# Register your receivers here
+from django.dispatch import receiver
+from django.urls import resolve
+from django.utils.translation import gettext_lazy as _
+from eventyay.control.signals import nav_event_settings
+
+@receiver(nav_event_settings, dispatch_uid="hubspot_nav")
+def navbar_info(sender, request, **kwargs):
+    url = resolve(request.path_info)
+    if not request.user.has_event_permission(
+        request.organizer, request.event, "can_change_event_settings", request=request
+    ):
+        return []
+    return [
+        {
+            "label": _("Hubspot"),
+            "active": url.namespace == "plugins:hubspot"
+            and url.url_name == "settings",
+        }
+    ]
